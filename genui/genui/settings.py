17bb0163278a0582a17137b5911f65157530d891
martin-sicho <sicho.martin@gmail.com>
2019-12-05 15:25:05 +0100

    removed pagination settings and changed url

--- genui/genui/settings.py
+++ genui/genui/settings.py
@@ -131,11 +131,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ],
-
-    # Pagination allows you to control how many objects per page are returned
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    # ]
 }
 
 # celery settings
--- genui/projects/urls.py
+++ genui/projects/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
         description="API for all things …",
         version="1.0.0"
     ), name='openapi-schema'),
-    path('swagger-ui/', TemplateView.as_view(
+    path('api/swagger-ui/', TemplateView.as_view(
         template_name='projects/swagger-ui.html',
         extra_context={'schema_url':'openapi-schema'}
     ), name='swagger-ui')
