5b5f4c8bfe35e610858d1415bec9ad1fd27cd54c
martin-sicho <sicho.martin@gmail.com>
2019-12-09 15:56:33 +0100

    remove CSRF protection for now

--- backend/genui/settings.py
+++ backend/genui/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware', FIXME: this should be handled once we approach more serious production level
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
