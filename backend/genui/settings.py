879df716ad71c1d738487825b73dae73a96d449f
martin-sicho <sicho.martin@gmail.com>
2019-12-20 17:30:22 +0100

    properly referenced redis in settings.py

--- backend/genui/settings.py
+++ backend/genui/settings.py
@@ -150,8 +150,8 @@ REST_FRAMEWORK = {
 }
 
 # celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379'
-# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+# CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_ACCEPT_CONTENT = ['application/json']
