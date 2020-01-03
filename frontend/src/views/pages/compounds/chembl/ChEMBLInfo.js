bfea5d9e44308277978c8c464112cc9dc3c39189
martin-sicho <sicho.martin@gmail.com>
2020-01-03 23:01:30 +0100

    added progress monitoring for tasks

--- frontend/src/views/pages/compounds/chembl/ChEMBLInfo.js
+++ frontend/src/views/pages/compounds/chembl/ChEMBLInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Badge, Col, Progress, Row } from 'reactstrap';
 
 class MolsStats extends React.Component {
 
@@ -39,15 +39,117 @@ class MolsStats extends React.Component {
   }
 }
 
-class MolSetImportStatus extends React.Component {
+function TaskHeadingBadge(props) {
+  if (props.tasks.length === 0) return null;
+
+  return (
+    <Badge {...props}>{props.tasks.length} {props.children}</Badge>
+  )
+}
+
+class TasksProgressOverview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progressData : []
+    }
+  }
+
+  componentDidMount() {
+    this.updateProgress();
+  }
+
+  updateProgress = () => {
+    const tasks = this.props.tasks;
+    tasks.forEach(task => {
+      const url = new URL(task.task_id + '/', this.props.progressURL);
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          data.task = task;
+          this.setState(state => {
+            return {
+              progressData : state.progressData.concat(data)
+            };
+          });
+        })
+      ;
+    });
+  };
 
   render() {
     return (
       <React.Fragment>
-        <h4>Started Tasks</h4>
-        <p>Something...</p>
-        <h4>Completed Tasks</h4>
-        <p>Something...</p>
+        {
+          this.state.progressData.map(data => (
+            <React.Fragment key={data.task.task_id}>
+              <div className="text-center">{data.task.task_name} ({data.progress.percent}%)</div>
+              <Progress value={data.progress.percent} />
+            </React.Fragment>
+          ))
+        }
+      </React.Fragment>
+    )
+  }
+}
+
+class MolSetTasksStatus extends React.Component {
+  intervalID;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks : null
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.intervalID);
+  }
+
+  componentDidMount() {
+    this.updateTasks();
+  }
+
+  updateTasks = () => {
+    fetch(this.props.tasksURL)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({tasks : {...data}});
+        this.intervalID = setTimeout(this.updateTasks.bind(this), 5000);
+      })
+  };
+
+  render() {
+    const tasks = this.state.tasks;
+    if (!tasks) {
+      return null
+    }
+
+    const completed = [];
+    const running = [];
+    const errors = [];
+    Object.keys(tasks).forEach(task_name => {
+      tasks[task_name].forEach(task => {
+        task.task_name = task_name;
+        if (task.status === 'SUCCESS') {
+          completed.push(task)
+        } else if (['STARTED', 'RECEIVED', 'PENDING', 'RETRY']) {
+          running.push(task)
+        } else if (['FAILURE', 'REVOKED'].includes(task.status)) {
+          errors.push(task)
+        }
+      });
+    });
+
+    return (
+      <React.Fragment>
+        <h4>
+          Tasks  <TaskHeadingBadge href="#" color="primary" tasks={completed}>Completed</TaskHeadingBadge> <TaskHeadingBadge href="#" color="primary" tasks={running}>Running</TaskHeadingBadge> <TaskHeadingBadge href="#" color="danger" tasks={errors}>Running</TaskHeadingBadge>
+        </h4>
+        <TasksProgressOverview {...this.props} tasks={running}/>
       </React.Fragment>
     )
   }
@@ -68,7 +170,7 @@ class ChEMBLInfo extends React.Component {
           <h4>Description</h4>
           <p>{this.molset.description}</p>
           <MolsStats molset={this.props.molset} moleculesURL={this.props.moleculesURL} />
-          <MolSetImportStatus molset={this.props.molset} tasksURL={this.props.tasksURL}/>
+          <MolSetTasksStatus progressURL={this.props.apiUrls.celeryProgress} molset={this.props.molset} tasksURL={this.props.tasksURL}/>
         </Col>
       </Row>
     );
