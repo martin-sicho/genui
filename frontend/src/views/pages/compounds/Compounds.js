56831756b4f8582f3c5a1b6265af5c7497e706d9
martin-sicho <sicho.martin@gmail.com>
2020-01-06 15:52:52 +0100

    added action menu to add compound sets

--- frontend/src/views/pages/compounds/Compounds.js
+++ frontend/src/views/pages/compounds/Compounds.js
@@ -1,5 +1,31 @@
 import React, {Component} from 'react';
 import ChEMBLGrid from './chembl/ChEMBLGrid';
+import { DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
+
+function HeaderNav(props) {
+  return (<UncontrolledDropdown nav inNavbar>
+    <DropdownToggle nav caret>
+      Actions
+    </DropdownToggle>
+    <DropdownMenu right>
+      <UncontrolledDropdown>
+        <DropdownToggle nav>Add New...</DropdownToggle>
+        <DropdownMenu>
+          {
+            props.molSetChoices.map(choice =>
+              (<DropdownItem
+                key={choice}
+                onClick={() => {props.onMolSetChoice(choice, [])}}
+              >
+                {choice}
+              </DropdownItem>)
+            )
+          }
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </DropdownMenu>
+  </UncontrolledDropdown>)
+}
 
 class Compounds extends Component {
 
@@ -20,7 +46,7 @@ class Compounds extends Component {
     this.state = {
       isLoading : true,
       fetchUpdates : true,
-      compoundSets : null
+      compoundSets : null,
     }
   }
 
@@ -52,7 +78,29 @@ class Compounds extends Component {
     })
   };
 
+  handleMolSetListAdd = (className, molsetList, overwrite=false) => {
+    this.setState((prev) => {
+      const old_sets = prev.compoundSets;
+      if (old_sets.hasOwnProperty(className)) {
+        if (overwrite) {
+          old_sets[className] = molsetList;
+        } else {
+          old_sets[className] = old_sets[className].concat(molsetList);
+        }
+      } else {
+        old_sets[className] = molsetList;
+      }
+      return {
+        compoundSets : old_sets
+      }
+    });
+  };
+
   // component methods
+
+  componentDidMount() {
+    this.props.onHeaderChange(<HeaderNav {...this.props} molSetChoices={Object.keys(this.CLASS_TO_COMPONENT)} onMolSetChoice={this.handleMolSetListAdd}/>);
+  }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
       this.fetchUpdates();
@@ -65,14 +113,27 @@ class Compounds extends Component {
       return <div>Loading...</div>
     }
 
+    if (Object.keys(molsets).length === 0) {
+      return <p>Start by selecting a compound set to create from the action menu.</p>
+    }
+
     return (
       <div className="compound-set-grids">
-        {Object.keys(molsets).map(MolSetClass => {
-          const MolsetComponent = this.CLASS_TO_COMPONENT[MolSetClass];
-          return (<div key={MolSetClass} className={MolSetClass}>
-            <MolsetComponent {...this.props} molsets={molsets[MolSetClass]}/>
-          </div>)
-        })}
+        {
+          Object.keys(molsets).map(MolSetClass => {
+            if (this.CLASS_TO_COMPONENT.hasOwnProperty(MolSetClass)) {
+              const MolsetComponent = this.CLASS_TO_COMPONENT[MolSetClass];
+              return (
+                <div key={MolSetClass} className={MolSetClass}>
+                  <MolsetComponent {...this.props} molsets={molsets[MolSetClass]}/>
+                </div>
+              )
+            } else {
+              console.log(`Unknown class: ${MolSetClass}`);
+              return null;
+            }
+          })
+        }
       </div>
     );
   }
