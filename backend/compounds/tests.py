db431d0a725b08593511772f2bda905b49aae0c9
martin-sicho <sicho.martin@gmail.com>
2020-01-06 15:24:15 +0100

    updates to tests

--- backend/compounds/tests.py
+++ backend/compounds/tests.py
@@ -13,31 +13,33 @@ class ChEMBLMolSetTestCase(TestCase):
             , "description" : "Test Description"
         })
 
-    # def test_populate_task(self):
-    #     molset = ChEMBLCompounds.objects.create(**{
-    #         "name": "Test ChEMBL Data Set",
-    #         "description": "Some description...",
-    #         "project": self.project
-    #     })
-    #     instance = MolSet.objects.get(pk=molset.id)
-    #     initializer = ChEMBLSetInitializer(instance, targets=["CHEMBL251"], max_per_target=1000)
-    #     initializer.populateInstance()
-    #     self.assertGreater(instance.molecules.count(), 0)
-    #     for err in initializer.errors:
-    #         self.assertTrue("Missing canonical SMILES string" in repr(err))
-
-
-    def test_populate_view(self):
-        populate_url = reverse('chemblSet-list')
-        response = self.client.post(populate_url, {
+    def test_populate_task(self):
+        molset = ChEMBLCompounds.objects.create(**{
             "name": "Test ChEMBL Data Set",
             "description": "Some description...",
-            "project": self.project.id,
-            "targets": [
-                "CHEMBL251",
-            ],
-            "maxPerTarget" : 1000
+            "project": self.project
         })
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(MolSet.objects.count(), 1)
-        self.assertEqual(ChEMBLCompounds.objects.count(), 1)
+        instance = MolSet.objects.get(pk=molset.id)
+        initializer = ChEMBLSetInitializer(instance, targets=["CHEMBL251"], max_per_target=500)
+        initializer.populateInstance()
+        self.assertGreater(instance.molecules.count(), 0)
+        for err in initializer.errors:
+            self.assertTrue("Missing canonical SMILES string" in repr(err))
+
+
+    # as of now this test affects the production database
+    # TODO: create a separate database and celery worker to test this properly
+    # def test_populate_view(self):
+    #     populate_url = reverse('chemblSet-list')
+    #     response = self.client.post(populate_url, {
+    #         "name": "Test ChEMBL Data Set",
+    #         "description": "Some description...",
+    #         "project": self.project.id,
+    #         "targets": [
+    #             "CHEMBL251",
+    #         ],
+    #         "maxPerTarget" : 1000
+    #     })
+    #     self.assertEqual(response.status_code, 201)
+    #     self.assertEqual(MolSet.objects.count(), 1)
+    #     self.assertEqual(ChEMBLCompounds.objects.count(), 1)
