556c2f5e025a4feeb0dd9517177471ecf8261c77
martin-sicho <sicho.martin@gmail.com>
2020-01-06 15:05:05 +0100

    updated init commands

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 # install docker beforehand
 docker run -p 6379:6379 --name genui-redis -d redis
-celery worker -A genui --loglevel=info &
-celery worker -c 2 -A genui --loglevel=info --queues gpu &
+celery worker -A -E genui --loglevel=info &
+celery worker -c 2 -A -E genui --loglevel=info --queues gpu &
