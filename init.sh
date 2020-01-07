1b1f416d2f7364a350d43ee6d57c6d43cd4510fe
martin-sicho <sicho.martin@gmail.com>
2020-01-07 21:12:56 +0100

    removed unneeded file

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# install docker beforehand
-docker run -p 6379:6379 --name genui-redis -d redis
-celery worker -A -E genui --loglevel=info &
-celery worker -c 2 -A -E genui --loglevel=info --queues gpu &
