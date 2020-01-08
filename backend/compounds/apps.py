d5dbfd92f73c704880e0eeaaf7c2960eb419af9d
martin-sicho <sicho.martin@gmail.com>
2020-01-08 11:59:17 +0100

    added a pre_delete signal which revokes all running or pending tasks before deletion of a compound set

--- backend/compounds/apps.py
+++ backend/compounds/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class CompoundsConfig(AppConfig):
     name = 'compounds'
+
+    def ready(self):
+        from compounds import signals
--- backend/compounds/signals.py
+++ backend/compounds/signals.py
@@ -6,10 +6,17 @@ On: 1/5/20, 5:40 PM
 """
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from celery import states
+from django.conf import settings
 
 from .models import MolSet
 
-@receiver(pre_delete, sender=MolSet)
-def delete_molset(sender, instance, using):
-    # TODO: find all running tasks connected tot his instance and revoke them
+@receiver(pre_delete, sender=MolSet, dispatch_uid='on_molset_delete_finish_tasks')
+def delete_molset_finish_tasks(sender, instance, using, **kwargs):
+    tasks = instance.tasks.all()
+    print(tasks)
+    for task in tasks:
+        task_id = task.task_id
+        if task.status not in (states.REVOKED, states.SUCCESS, states.FAILURE):
+            settings.CURRENT_CELERY_INSTANCE.control.revoke(task_id=task_id, terminate=True)
     print('About to delete: ', repr(instance))
