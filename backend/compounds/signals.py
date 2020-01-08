c76067f0ad750dd0e763596e01e67c1f500b45bd
martin-sicho <sicho.martin@gmail.com>
2020-01-08 19:20:35 +0100

    remove prints

--- backend/compounds/signals.py
+++ backend/compounds/signals.py
@@ -14,9 +14,7 @@ from .models import MolSet
 @receiver(pre_delete, sender=MolSet, dispatch_uid='on_molset_delete_finish_tasks')
 def delete_molset_finish_tasks(sender, instance, using, **kwargs):
     tasks = instance.tasks.all()
-    print(tasks)
     for task in tasks:
         task_id = task.task_id
         if task.status not in (states.REVOKED, states.SUCCESS, states.FAILURE):
             settings.CURRENT_CELERY_INSTANCE.control.revoke(task_id=task_id, terminate=True)
-    print('About to delete: ', repr(instance))
