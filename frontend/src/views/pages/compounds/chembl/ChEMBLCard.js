df9dc81f817722dd03d789c94c402885db23f019
martin-sicho <sicho.martin@gmail.com>
2020-01-09 13:39:09 +0100

    moved tab widget to the common genui package

--- frontend/src/views/pages/compounds/chembl/ChEMBLCard.js
+++ frontend/src/views/pages/compounds/chembl/ChEMBLCard.js
@@ -1,7 +1,7 @@
 import { Button, CardBody, CardFooter, CardHeader, CardSubtitle } from 'reactstrap';
 import React from 'react';
 import './styles.css';
-import TabWidget from './TabWidget';
+import TabWidget from '../../../../genui/components/TabWidget';
 import ChEMBLInfo from './ChEMBLInfo';
 import ChEMBLCompounds from './ChEMBLCompounds';
 
