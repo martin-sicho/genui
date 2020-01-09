6c2eebebb7c3f6b7a2dd08915b0d826ae7faa530
martin-sicho <sicho.martin@gmail.com>
2020-01-09 22:10:30 +0100

    refactored so that functionality related to task progress is decoupled in the genui package

--- frontend/src/genui/index.js
+++ frontend/src/genui/index.js
@@ -1,11 +1,15 @@
 import RoutedPage from "./components/RoutedPage";
 import ResponsiveGrid from "./components/ResponsiveGrid";
 import TabWidget from './components/TabWidget';
-import TaskAwareComponent from './components/TaskAwareComponent';
+import TaskAwareComponent from './components/tasks/TaskAwareComponent';
+import TaskBadgeGroup from './components/tasks/TaskBadgeGroup';
+import TaskProgressBar from './components/tasks/TaskProgressBar';
 
 export {
   RoutedPage,
   ResponsiveGrid,
   TabWidget,
-  TaskAwareComponent
+  TaskAwareComponent,
+  TaskBadgeGroup,
+  TaskProgressBar
 }
--- frontend/src/views/pages/compounds/MolSetTasks.js
+++ frontend/src/views/pages/compounds/MolSetTasks.js
@@ -1,72 +1,5 @@
 import React from 'react';
-import { Badge, Progress } from 'reactstrap';
-
-function TaskHeadingBadge(props) {
-  if (props.tasks.length === 0) return null;
-
-  return (
-    <Badge {...props}>{props.tasks.length} {props.children}</Badge>
-  )
-}
-
-class TasksProgressOverview extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      progressData : []
-    }
-  }
-
-  componentDidMount() {
-    this.updateProgress();
-    this.interval = setInterval(this.updateProgress, 2000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  updateProgress = () => {
-    const tasks = this.props.tasks;
-    const progressData = [];
-    tasks.forEach(task => {
-      const url = new URL(task.task_id + '/', this.props.progressURL);
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          data.task = task;
-          progressData.push(data);
-
-          // FIXME: this set state should not happen if the component is unmounted -> the fetch needs to be cancelled properly
-          this.setState(state => {
-            return {
-              progressData : progressData
-            };
-          });
-        })
-      ;
-    });
-  };
-
-  render() {
-    const progress = this.state.progressData;
-    progress.sort((a, b) => (a.task.task_id > b.task.task_id) ? 1 : -1);
-
-    return (
-      <React.Fragment>
-        {
-          progress.map(data => (
-            <React.Fragment key={data.task.task_id}>
-              <div className="text-center">{data.task.task_name} ({data.progress.percent}%)</div>
-              <Progress value={data.progress.percent} />
-            </React.Fragment>
-          ))
-        }
-      </React.Fragment>
-    )
-  }
-}
+import {TaskBadgeGroup, TaskProgressBar} from '../../../genui'
 
 class MolSetTasks extends React.Component {
 
@@ -79,13 +12,12 @@ class MolSetTasks extends React.Component {
     return (
       <React.Fragment>
         <h4>
-          Tasks <TaskHeadingBadge href="#" color="primary" tasks={tasks.running}>Running</TaskHeadingBadge> <TaskHeadingBadge href="#" color="success" tasks={tasks.completed}>Completed</TaskHeadingBadge> <TaskHeadingBadge href="#" color="danger" tasks={tasks.errors}>Failed</TaskHeadingBadge>
+          Tasks <TaskBadgeGroup tasks={tasks}/>
         </h4>
-        {
-          tasks.running.length > 0 ?
-            <TasksProgressOverview {...this.props} tasks={tasks.running}/>
-            : null
-        }
+        <TaskProgressBar
+          progressURL={this.props.progressURL}
+          tasks={tasks.running}
+        />
       </React.Fragment>
     )
   }
