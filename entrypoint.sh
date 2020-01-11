2e4fe532e6ef3058d4fb5d203d1c5e4ac02c0e37
martin-sicho <sicho.martin@gmail.com>
2020-01-11 18:25:14 +0100

    migrate the django-rdkit app first to avoid problems

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 set -e
 
 npm run-script build --prefix /code/frontend
+python /code/backend/manage.py migrate --noinput django_rdkit
 python /code/backend/manage.py migrate --noinput
 python /code/backend/manage.py runserver 0.0.0.0:8000
