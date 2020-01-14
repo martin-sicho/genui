1d6749ec79d9d0850f8b188bb91d05f741da2ef4
martin-sicho <sicho.martin@gmail.com>
2020-01-14 16:39:10 +0100

    remove irrelevant line

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@
 set -e
 
 npm run-script build --prefix /code/frontend
-python /code/backend/manage.py migrate --noinput django_rdkit # FIXME: specify a dependency in the migrations of the compounds app rather than this
 python /code/backend/manage.py migrate --noinput
 python /code/backend/manage.py runserver 0.0.0.0:8000
