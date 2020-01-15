34393f5eaafa59f972d45d00566eccdba2fb9029
martin-sicho <sicho.martin@gmail.com>
2020-01-15 10:44:51 +0100

    moved the dynamic initialization code to the ready function of the app config

--- backend/qsar/apps.py
+++ backend/qsar/apps.py
@@ -2,3 +2,16 @@ from django.apps import AppConfig
 
 class QsarConfig(AppConfig):
     name = 'qsar'
+
+    def ready(self):
+        from .algorithms import bases
+        from commons.helpers import getSubclassesFromModule
+        from .algorithms import algorithms as algs
+        from .algorithms import metrics as metrics
+
+        for x in getSubclassesFromModule(bases.BaseAlgorithm, algs):
+            x.getParams()
+            x.getDjangoModel()
+
+        for x in getSubclassesFromModule(bases.ValidationMetric, metrics):
+            x.getDjangoModel()
--- backend/qsar/views.py
+++ backend/qsar/views.py
@@ -2,13 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets, mixins, pagination
 
-from commons.helpers import getSubclassesFromModule
 from . import models
 from . import serializers
-
-from .algorithms import bases
-from .algorithms import algorithms as algs
-from .algorithms import metrics as metrics
 
 class PerformancePagination(pagination.PageNumberPagination):
     page_size = 10
@@ -22,8 +17,6 @@ class MetricsViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet
 ):
-    METRICS_ALL = [x.getDjangoModel() for x in getSubclassesFromModule(bases.ValidationMetric, metrics)]
-
     queryset = models.ModelPerformanceMetric.objects.all()
     serializer_class = serializers.ModelPerformanceMetricSerializer
 
@@ -32,8 +25,6 @@ class AlgorithmViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet
 ):
-    ALGORITHMS_ALL = [x.getDjangoModel() for x in getSubclassesFromModule(bases.BaseAlgorithm, algs)]
-
     queryset = models.Algorithm.objects.all()
     serializer_class = serializers.AlgorithmSerializer
 
