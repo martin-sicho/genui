a5c5feae1bdd0c20b0f29fb2c9a2a85ce49108f9
martin-sicho <sicho.martin@gmail.com>
2020-01-17 17:43:35 +0100

    add urls to serve media files in development

--- backend/genui/urls.py
+++ backend/genui/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
@@ -44,6 +46,7 @@ urlpatterns = [
     re_path(r'^api/(swagger/)?$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     # if it is not a direct request to backend, serve the frontend app
     path('', views.FrontendAppView.as_view()),
     re_path(r'^(?:.*)/?$', views.FrontendAppView.as_view())
