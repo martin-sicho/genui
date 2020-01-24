a215c544c64b45e5ac1bb342e0e29332d8976a2f
martin-sicho <sicho.martin@gmail.com>
2020-01-24 17:24:33 +0100

    more methods moved to the common model builder base class

--- backend/modelling/core/bases.py
+++ backend/modelling/core/bases.py
@@ -9,7 +9,11 @@ from abc import ABC, abstractmethod
 import joblib
 from pandas import DataFrame, Series
 
-import modelling.models
+import uuid
+from django.core.files.base import ContentFile
+
+from modelling import models
+from commons.helpers import findClassInModule
 
 
 class Algorithm(ABC):
@@ -22,19 +26,19 @@ class Algorithm(ABC):
         return [Algorithm.CLASSIFICATION, Algorithm.REGRESSION]
 
     @classmethod
-    def getDjangoModel(cls) -> modelling.models.Algorithm:
+    def getDjangoModel(cls) -> models.Algorithm:
         if not cls.name:
             raise Exception('You have to specify a name for the algorithm in its class "name" property')
-        ret = modelling.models.Algorithm.objects.get_or_create(
+        ret = models.Algorithm.objects.get_or_create(
             name=cls.name
         )[0]
-        file_format = modelling.models.ModelFileFormat.objects.get_or_create(
+        file_format = models.ModelFileFormat.objects.get_or_create(
             fileExtension=".joblib.gz",
             description="A compressed joblib file."
         )[0]
         ret.fileFormats.add(file_format)
         for mode in cls.getModes():
-            mode = modelling.models.AlgorithmMode.objects.get_or_create(name=mode)[0]
+            mode = models.AlgorithmMode.objects.get_or_create(name=mode)[0]
             ret.validModes.add(mode)
         ret.save()
         return ret
@@ -86,7 +90,7 @@ class ValidationMetric(ABC):
     def getDjangoModel(cls):
         if not cls.name:
             raise Exception('You have to specify a name for the validation metric in its class "name" property')
-        ret = modelling.models.ModelPerformanceMetric.objects.get_or_create(
+        ret = models.ModelPerformanceMetric.objects.get_or_create(
             name=cls.name
         )[0]
         if cls.description:
@@ -97,3 +101,82 @@ class ValidationMetric(ABC):
     @abstractmethod
     def __call__(self, true_vals : Series, predicted_vals : Series):
         pass
+
+class ModelBuilder(ABC):
+
+    @staticmethod
+    def findAlgorithmClass(name):
+        from . import algorithms
+        return findClassInModule(Algorithm, algorithms, "name", name)
+
+    @staticmethod
+    def findMetricClass(name):
+        from . import metrics
+        return findClassInModule(ValidationMetric, metrics, "name", name)
+
+    def __init__(
+            self,
+            instance : models.Model,
+            progress = None,
+            onFit = None
+    ):
+        self.instance = instance
+
+        self.training = self.instance.trainingStrategy
+        self.algorithmClass = self.findAlgorithmClass(self.training.algorithm.name)
+        self.onFit = onFit
+
+        self.validation = self.instance.validationStrategy
+        self.metricClasses = [self.findMetricClass(x.name) for x in self.validation.metrics.all()]
+
+        self.progress = progress
+        self.progressStages = []
+        self.currentProgress = 0
+        self.errors = []
+
+        self._model = None
+
+
+    @property
+    def model(self) -> Algorithm:
+        return self._model
+
+    @model.setter
+    def model(self, val):
+        self._model = val
+
+    @abstractmethod
+    def getY(self) -> Series:
+        pass
+
+    @abstractmethod
+    def getX(self) -> DataFrame:
+        pass
+
+    def recordProgress(self):
+        if self.currentProgress < len(self.progressStages):
+            if self.progress:
+                self.progress.set_progress(
+                    self.currentProgress
+                    , len(self.progressStages)
+                    , description=self.progressStages[self.currentProgress]
+                )
+            print(self.progressStages[self.currentProgress])
+        else:
+            self.errors.append(Exception("Incorrect progress count detected."))
+        self.currentProgress += 1
+        print(f"{self.currentProgress}/{len(self.progressStages)}")
+
+    def fit(self, callback=None) -> models.Model:
+        # TODO: sanity check if length of X and y are the same
+        self.model = self.algorithmClass(self, callback if callback else self.onFit)
+        self.model.fit(self.getX(), self.getY())
+        self.saveFile(self.model)
+        return self.instance
+
+    def saveFile(self, model : Algorithm):
+        name = f"{self.algorithmClass.name}{self.instance.id}_project{self.instance.project.id}_{uuid.uuid1()}"
+        extension = model.fileFormat.fileExtension
+        self.instance.modelFile.save(name + extension, ContentFile('Dummy file for {0}'.format(name)))
+        path = self.instance.modelFile.path.replace(extension, '')
+        model.serialize(path)
--- backend/qsar/algorithms/bases.py
+++ backend/qsar/algorithms/bases.py
@@ -4,12 +4,10 @@ algorithms
 Created by: Martin Sicho
 On: 14-01-20, 10:16
 """
-import uuid
-from django.core.files.base import ContentFile
 
 import modelling.models
 from commons.helpers import findClassInModule
-from modelling.core.bases import Algorithm, ValidationMetric
+from modelling.core.bases import Algorithm, ModelBuilder
 from qsar import models
 import pandas as pd
 from pandas import DataFrame, Series
@@ -31,61 +29,31 @@ class DescriptorCalculator:
         return models.DescriptorGroup.objects.get_or_create(name=cls.group_name)[0]
 
 
-class QSARModelBuilder:
-    @staticmethod
-    def findAlgorithmClass(name):
-        from . import algorithms
-        return findClassInModule(Algorithm, algorithms, "name", name)
-
-    @staticmethod
-    def findMetricClass(name):
-        from . import metrics
-        return findClassInModule(ValidationMetric, metrics, "name", name)
+class QSARModelBuilder(ModelBuilder):
 
     @staticmethod
     def findDescriptorClass(name):
         from . import descriptors
         return findClassInModule(DescriptorCalculator, descriptors, "group_name", name)
 
-    def __init__(
-            self,
-            instance : models.QSARModel,
-            progress = None,
-            onFitCall=None
-    ):
-        self.instance = instance
-        self.training = self.instance.trainingStrategy
-        self.validation = self.instance.validationStrategy
-        self.descriptorClasses = [self.findDescriptorClass(x.name) for x in self.training.descriptors.all()]
-        self.algorithmClass = self.findAlgorithmClass(self.training.algorithm.name)
-        self.metricClasses = [self.findMetricClass(x.name) for x in self.validation.metrics.all()]
-        self.model = None
-        self.onFitCall = onFitCall
+    def __init__(self, instance: models.QSARModel, progress=None, onFitCall=None):
+        super().__init__(instance, progress, onFitCall)
+
         self.molset = self.instance.molset
-        self.progress = progress
+        self.descriptorClasses = [self.findDescriptorClass(x.name) for x in self.training.descriptors.all()]
+
         self.X = None
         self.y = None
-        self.progressStages = []
-        self.currentProgress = 0
-        self.errors = []
 
-    def recordProgress(self):
-        if self.currentProgress < len(self.progressStages):
-            if self.progress:
-                self.progress.set_progress(
-                    self.currentProgress
-                    , len(self.progressStages)
-                    , description=self.progressStages[self.currentProgress]
-                )
-            print(self.progressStages[self.currentProgress])
-        else:
-            self.errors.append(Exception("Incorrect progress count detected."))
-        self.currentProgress += 1
-        print(f"{self.currentProgress}/{len(self.progressStages)}")
+    def getX(self) -> DataFrame:
+        return self.X
+
+    def getY(self) -> Series:
+        return self.y
 
     def calculateDescriptors(self, mols):
         smiles = [x.canonicalSMILES for x in mols]
-        if not self.X:
+        if not self.getX():
             self.X = DataFrame()
             for desc_class in self.descriptorClasses:
                 calculator = desc_class(self)
@@ -95,7 +63,7 @@ class QSARModelBuilder:
             return self.X
 
     def saveActivities(self):
-        if not self.y:
+        if not self.getY():
             activity_set = self.instance.molset.activities.get()
             compounds, activities = activity_set.cleanForModelling()
             activities = Series(activities)
@@ -104,13 +72,6 @@ class QSARModelBuilder:
                 activities = activities.apply(lambda x : 1 if x >= activity_thrs else 0)
             self.y = activities
             return self.y, compounds
-
-    def fit(self, callback=None) -> models.QSARModel:
-        # TODO: sanity check if length of X and y are the same
-        self.model = self.algorithmClass(self, callback if callback else self.onFitCall)
-        self.model.fit(self.X, self.y)
-        self.saveFile()
-        return self.instance
 
     def fitValidate(self) -> models.QSARModel:
         ret = self.fit()
@@ -129,10 +90,3 @@ class QSARModelBuilder:
                     model=self.instance,
                     **kwargs
                 )
-
-    def saveFile(self):
-        name = f"{self.algorithmClass.name}{self.instance.id}_project{self.instance.project.id}_{uuid.uuid1()}"
-        extension = self.model.fileFormat.fileExtension
-        self.instance.modelFile.save(name + extension, ContentFile('Dummy file for {0}'.format(name)))
-        path = self.instance.modelFile.path.replace(extension, '')
-        self.model.serialize(path)
