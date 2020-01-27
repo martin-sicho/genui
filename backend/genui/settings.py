ace062b48815e7f293a1e916c164ddf59d7b772b
martin-sicho <sicho.martin@gmail.com>
2020-01-27 12:06:38 +0100

    added generators to settings

--- backend/genui/settings.py
+++ backend/genui/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'compounds.apps.CompoundsConfig',
     'modelling.apps.ModellingConfig',
     'qsar.apps.QsarConfig',
+    'generators.apps.GeneratorsConfig'
 ]
 
 MIDDLEWARE = [
