0ed74c5bbe4a163ddbc955b400b9e2414435fd1a
martin-sicho <sicho.martin@gmail.com>
2020-02-27 18:12:27 +0100

    implemented a method to recursively get all paginated point data

--- frontend/src/views/pages/maps/display/Map.js
+++ frontend/src/views/pages/maps/display/Map.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import './plot-styles.css';
+import withUnmounted from '@ishawnwang/withunmounted';
 
-export default function Map(props) {
+function MapPlot(props) {
+
   let trace1 = {
     x: [1, 2, 3, 4],
     y: [10, 15, 13, 17],
@@ -27,7 +29,7 @@ export default function Map(props) {
   let data = [trace1, trace2, trace3];
 
   let layout = {
-    title: 'Responsive to window\'s size!',
+    title: 'Responsive',
     font: {size: 18},
     autosize: true
   };
@@ -46,3 +48,96 @@ export default function Map(props) {
     </div>
   )
 }
+
+class Map extends React.Component {
+  abort = new AbortController();
+  hasUnmounted = false;
+
+  constructor(props) {
+    super(props);
+
+    this.map = this.props.map;
+    this.molsets = this.map.molsets;
+    this.pointsUrl = new URL(`${this.map.id}/points/`, this.props.apiUrls.mapsRoot);
+
+    this.state = {
+      points : {},
+      lastPage : null,
+      nextPage : this.pointsUrl,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData(this.state.nextPage);
+  }
+
+  componentWillUnmount() {
+    this.abort.abort();
+  }
+
+  fetchData = (page) => {
+    fetch(page, {signal : this.abort.signal})
+      .then(response => this.props.handleResponseErrors(response, `Cannot fetch page: ${page.toString()}`))
+      .then(data => {
+        const pagePoints = {};
+        data.results.forEach(
+          result => {
+            const providers = [];
+            this.molsets.forEach(molset => {
+              if (result.molecule.providers.includes(molset.id)) {
+                providers.push(molset);
+              }
+            });
+            providers.forEach(provider => {
+              if (!pagePoints.hasOwnProperty(provider.id)) {
+                pagePoints[provider.id] = [];
+              }
+              pagePoints[provider.id].push(result)
+            })
+          }
+        );
+
+        if (this.hasUnmounted) {
+          return
+        }
+
+        let nextPage = null;
+        if (data.next) {
+          nextPage = new URL(data.next);
+          this.fetchData(new URL(data.next));
+        }
+
+        this.setState(prevState => {
+          const points = prevState.points;
+          Object.keys(pagePoints).forEach(providerID => {
+            if (points[providerID]) {
+              pagePoints[providerID].forEach(result =>
+                points[providerID].push(result)
+              );
+            } else {
+              points[providerID] = pagePoints[providerID];
+            }
+          });
+
+          return {
+            points : points,
+            lastPage: page,
+            nextPage: nextPage,
+          }
+        })
+      })
+      .catch(e => console.log(e))
+  };
+
+  render() {
+    return (
+      <MapPlot
+        {...this.props}
+        molsets={this.molsets}
+        points={this.state.points}
+      />
+    )
+  }
+}
+
+export default withUnmounted(Map);
--- frontend/src/views/pages/maps/display/MapDashboard.js
+++ frontend/src/views/pages/maps/display/MapDashboard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Map from './Map';
 import { ComponentWithObjects } from '../../../../genui';
 import MapsPage from './MapPage';
 
