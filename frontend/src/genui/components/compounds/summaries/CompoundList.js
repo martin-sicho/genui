e446632ccc02cf1c8a6ec0ff9a688e34ba8a80d9
martin-sicho <sicho.martin@gmail.com>
2020-03-09 14:19:09 +0100

    decouple compound list item from the list itself

--- frontend/src/genui/components/compounds/summaries/CompoundList.js
+++ frontend/src/genui/components/compounds/summaries/CompoundList.js
@@ -2,6 +2,27 @@ import React from 'react';
 import { Col, Row } from 'reactstrap';
 import { ActivitySetList, MoleculeActivityDetail, MoleculeData, MoleculeDetail } from '../../..';
 
+export function CompoundListItem(props) {
+  const mol = props.mol;
+  return (
+    <Row>
+      <Col md={3} sm={3}>
+        <MoleculeDetail mol={mol}/>
+      </Col>
+      <Col md={3} sm={3}>
+        <MoleculeData {...props} mol={mol}/>
+      </Col>
+      <Col md={6} sm={6}>
+        <MoleculeActivityDetail
+          {...props}
+          mol={mol}
+          component={ActivitySetList}
+        />
+      </Col>
+    </Row>
+  )
+}
+
 export default function CompoundList(props) {
   const mols = props.mols;
 
@@ -9,21 +30,7 @@ export default function CompoundList(props) {
     <React.Fragment>
       {
         mols.map(mol => (
-          <Row key={mol.id}>
-            <Col md={3} sm={3}>
-              <MoleculeDetail mol={mol}/>
-            </Col>
-            <Col md={3} sm={3}>
-              <MoleculeData {...props} mol={mol}/>
-            </Col>
-            <Col md={6} sm={6}>
-              <MoleculeActivityDetail
-                {...props}
-                mol={mol}
-                component={ActivitySetList}
-              />
-            </Col>
-          </Row>
+          <CompoundListItem {...props} key={mol.id} mol={mol}/>
         ))
       }
     </React.Fragment>
