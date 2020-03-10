eb0ad80ed0f572a6f194fa6cc0881bd71a073dbe
martin-sicho <sicho.martin@gmail.com>
2020-03-10 11:38:13 +0100

    moved pagination above the items

--- frontend/src/genui/components/ApiResourcePaginator.js
+++ frontend/src/genui/components/ApiResourcePaginator.js
@@ -43,16 +43,16 @@ class ApiResourcePaginator extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.children(this.state.activePageItems)}
         <Pagination
           activePage={this.state.activePage}
-          itemsCountPerPage={this.props.itemsPerPage ? this.props.itemsPerPage : 10}
+          // itemsCountPerPage={this.props.itemsPerPage ? this.props.itemsPerPage : 10}
           totalItemsCount={this.state.totalCount}
           pageRangeDisplayed={5}
           onChange={this.handlePageChange.bind(this)}
           itemClass="page-item"
           linkClass="page-link"
         />
+        {this.props.children(this.state.activePageItems)}
       </React.Fragment>
     );
   }
