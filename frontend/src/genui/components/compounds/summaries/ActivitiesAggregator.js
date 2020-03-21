d49ec5d9dd3d55f59c73403e31cc4ca6cdb8b85f
martin-sicho <sicho.martin@gmail.com>
2020-03-21 16:54:53 +0100

    add an aggregator of activities for molecule sets

---
+++ frontend/src/genui/components/compounds/summaries/ActivitiesAggregator.js
@@ -0,0 +1,74 @@
+import { ComponentWithPagedResources, MolsToMolSetGroups } from '../../../../genui';
+import React from 'react';
+
+export default function ActivitiesAggregator(props) {
+  return (
+    <MolsToMolSetGroups
+      {...props}
+    >
+      {
+        (groups) => {
+          const groupedMols = groups;
+          const definition = {};
+          // const molsetToActivities = {};
+          Object.keys(groupedMols).forEach(molsetID => {
+            const molset = props.molsets.find(molset => molset.id === Number(molsetID));
+            const mols =  groupedMols[molsetID].map(mol => mol.mol);
+            // molsetToActivities[molsetID] = {};
+            // molsetToActivities[molsetID]['molecules'] = mols;
+            // molsetToActivities[molsetID]['molset'] = molset;
+            // molsetToActivities[molsetID]['acsets'] = {};
+            Object.keys(props.activitySets).forEach(key => {
+              if (molset.activities.includes(Number(key))) {
+                // molsetToActivities[molsetID]['acsets'][key] = {};
+                // molsetToActivities[molsetID]['acsets'][key]['acset'] = props.activitySets[key];
+                // molsetToActivities[molsetID]['acsets'][key]['activities'] = [];
+                definition[key] = new URL(`${key}/activities/?mols=${mols.map(mol => mol.id).join(',')}`, props.apiUrls.activitySetsRoot);
+              }
+            });
+          });
+
+          // console.log(definition);
+          return (
+            <ComponentWithPagedResources
+              {...props}
+              definition={definition}
+              updateCondition={props.resourceUpdateCondition}
+            >
+              {
+                (data, finished, revision, revisionFinished) => {
+                  if (finished) {
+                    // console.log(finished);
+                    // console.log(revision);
+                    // console.log(revisionFinished);
+                    // console.log(data);
+                    // console.log('----');
+
+                    const activitiesAll = [];
+                    Object.keys(data).forEach(key => {
+                      data[key].forEach(item => activitiesAll.push(item))
+                    });
+
+                    // Object.keys(groupedMols).forEach(molsetID => {
+                    //   Object.keys(molsetToActivities[molsetID]['acsets']).forEach(acsetID => {
+                    //     let activities = data[acsetID];
+                    //     activities = groupBy(activities, 'type.id');
+                    //     molsetToActivities[molsetID]['acsets'][acsetID]['activities'] = activities;
+                    //   });
+                    // });
+
+                    // console.log(activitiesAll);
+
+                    return props.children(activitiesAll);
+                  } else {
+                    return props.children(null)
+                  }
+                }
+              }
+            </ComponentWithPagedResources>
+          )
+        }
+      }
+    </MolsToMolSetGroups>
+  )
+}
