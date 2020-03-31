6a6b8be7f0364513649e8d63ce0b515a31ce2de5
martin-sicho <sicho.martin@gmail.com>
2020-03-31 13:28:20 +0200

    make sure that we do not run this for commands that might not have the database or apps initialized

--- backend/commons/helpers.py
+++ backend/commons/helpers.py
@@ -7,6 +7,7 @@ On: 14-01-20, 17:25
 
 import inspect
 import logging
+import sys
 
 from django.contrib.auth.models import Group, Permission
 
@@ -59,21 +60,22 @@ def createGroup(
         , overwrite=True
         , appendPermissions=True
 ):
-    group, created = Group.objects.get_or_create(name=groupName)
-    if not overwrite and not created:
-        raise Exception(f"Group {groupName} already exists and overwrite is off.")
-    if not appendPermissions:
-        group.permissions.clear()
+    if len(sys.argv) > 1 and sys.argv[1] not in ('makemigrations', 'sqlmigrate', 'migrate', "test"):
+        group, created = Group.objects.get_or_create(name=groupName)
+        if not overwrite and not created:
+            raise Exception(f"Group {groupName} already exists and overwrite is off.")
+        if not appendPermissions:
+            group.permissions.clear()
 
-    for model in models:
-        for permission in permissions:
-            codename = f"{permission}_{model.__name__.lower()}"
-            print(f"Creating permission for group {groupName}: {codename}")
+        for model in models:
+            for permission in permissions:
+                codename = f"{permission}_{model.__name__.lower()}"
+                print(f"Creating permission for group {groupName}: {codename}")
 
-            try:
-                model_add_perm = Permission.objects.get(codename=codename)
-            except Permission.DoesNotExist:
-                logging.warning("Permission not found with name '{}'.".format(codename))
-                continue
+                try:
+                    model_add_perm = Permission.objects.get(codename=codename)
+                except Permission.DoesNotExist:
+                    logging.warning("Permission not found with name '{}'.".format(codename))
+                    continue
 
-            group.permissions.add(model_add_perm)
+                group.permissions.add(model_add_perm)
