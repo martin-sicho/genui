e269bc2a909b6b4f7f465aaf8a71a684de6f7a71
martin-sicho <sicho.martin@gmail.com>
2020-04-13 10:44:58 +0200

    allow specification of update condition through a prop

--- frontend/src/genui/components/compounds/tabs/MolsInMolSetList.js
+++ frontend/src/genui/components/compounds/tabs/MolsInMolSetList.js
@@ -4,6 +4,12 @@ import React from 'react';
 
 export default function MolsInMolSetList(props) {
   const molset = props.molset;
+  const updateCondition = props.updateCondition ? props.updateCondition : (prevProps, nextProps) => {
+    // console.log(prevProps.tasksRunning);
+    // console.log(nextProps.tasksRunning);
+    // console.log('xxx');
+    return prevProps.tasksRunning !== nextProps.tasksRunning;
+  };
   return (
     <Row>
       <Col sm="12">
@@ -12,12 +18,7 @@ export default function MolsInMolSetList(props) {
           {...props}
           activitySetsIDs={molset.activities}
           showInfo={props.showInfo}
-          updateCondition={(prevProps, nextProps) => {
-            // console.log(prevProps.tasksRunning);
-            // console.log(nextProps.tasksRunning);
-            // console.log('xxx');
-            return prevProps.tasksRunning !== nextProps.tasksRunning;
-          }}
+          updateCondition={updateCondition}
         />
       </Col>
     </Row>
