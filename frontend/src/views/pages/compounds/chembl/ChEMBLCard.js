f8ce61007f963b35d4bd746b676bf9013aa6a521
martin-sicho <sicho.martin@gmail.com>
2020-04-14 15:04:18 +0200

    add more information to compounds details in the app

--- frontend/src/views/pages/compounds/chembl/ChEMBLCard.js
+++ frontend/src/views/pages/compounds/chembl/ChEMBLCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChEMBLInfo from './tabs/ChEMBLInfo';
 import { GenericMolSetCard, MolsInMolSetList } from '../../../../genui';
+import { AssayName, ChEMBLID, Relation, TargetName } from './ActivityFields';
 
 function ChEMBLCard(props) {
   const tabs = [
@@ -10,7 +11,47 @@ function ChEMBLCard(props) {
     },
     {
       title: "Compounds",
-      renderedComponent : (props) => <MolsInMolSetList {...props} showInfo={true}/>
+      renderedComponent : (props) => (
+        <MolsInMolSetList
+          {...props}
+          showInfo={true}
+          extraActivityFields={
+            [
+              {
+                dataItems: ["extraArgs.relation"],
+                propNames: ["relation"],
+                displayName: "Relation",
+                component: Relation,
+                className: "ChEMBLActivity"
+              },
+              {
+                dataItems: ["extraArgs.assay"],
+                propNames: ["assayID"],
+                displayName: "Assay",
+                component: AssayName,
+                className: "ChEMBLActivity"
+              },
+              {
+                dataItems: ["extraArgs.target"],
+                propNames: ["targetID"],
+                displayName: "Target",
+                component: TargetName,
+                className: "ChEMBLActivity"
+              },
+            ]
+          }
+          extraInfoFields={
+            [
+              {
+                dataItems: ["extraArgs.chemblID"],
+                propNames: ["compoundID"],
+                displayName: "ChEMBL ID",
+                component: ChEMBLID,
+                className: "ChEMBLMolecule"
+              }
+            ]
+          }
+        />)
     }
   ];
 
