b81bbc6cfdf481e1d2924ee4ab6cac0a855b6472
martin-sicho <sicho.martin@gmail.com>
2020-04-24 14:08:46 +0200

    remove react app static files from settings.py

--- src/genui/settings.py
+++ src/genui/settings.py
@@ -197,10 +197,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-REACT_APP_DIR = os.path.join(BASE_DIR, '../frontend')
-STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static'),
-]
+# REACT_APP_DIR = os.path.join(BASE_DIR, '../frontend')
+# STATICFILES_DIRS = [
+#     os.path.join(REACT_APP_DIR, 'build', 'static'),
+# ]
 
 # rest framework
 REST_FRAMEWORK = {
