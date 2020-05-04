f2ad89ff52a404a9813a58ed49bdfacf4ac16170
martin-sicho <sicho.martin@gmail.com>
2020-05-04 10:55:16 +0200

    production settings as default

--- src/genui/wsgi.py
+++ src/genui/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genui.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genui.settings.prod')
 
 application = get_wsgi_application()
