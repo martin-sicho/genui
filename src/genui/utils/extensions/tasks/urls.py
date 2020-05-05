936bd3fab631fc5df7b78faae8f3cc2662fd3885
martin-sicho <sicho.martin@gmail.com>
2020-05-05 10:16:06 +0200

    change task progress url

--- src/genui/utils/extensions/tasks/urls.py
+++ src/genui/utils/extensions/tasks/urls.py
@@ -9,5 +9,5 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^api/celery-progress/(?P<task_id>[\w-]+)/$', views.TaskProgressView.as_view())
+    re_path(r'^api/tasks/progress/(?P<task_id>[\w-]+)/$', views.TaskProgressView.as_view())
 ]
