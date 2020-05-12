297805daf0dbef965c94a4af134c3d9b748bf3a4
martin-sicho <sicho.martin@gmail.com>
2020-05-12 11:01:57 +0200

    add the list of permissions to admin

--- src/genui/accounts/admin.py
+++ src/genui/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
-# Register your models here.
+
+@admin.register(Permission)
+class PermissionsAdmin(admin.ModelAdmin):
+    pass
