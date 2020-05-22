61b8cf3cd136a43f7e8a1b16fa440a330008b23d
martin-sicho <sicho.martin@gmail.com>
2020-05-22 16:58:23 +0200

    get the host from a variable if available

--- src/genui/settings/databases/prod.py
+++ src/genui/settings/databases/prod.py
@@ -12,7 +12,7 @@ DATABASES = {
         'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': 'db',
+        'HOST': os.environ['POSTGRES_HOST'] if 'POSTGRES_HOST' in os.environ else 'db',
         'PORT': 5432,
     }
 }
