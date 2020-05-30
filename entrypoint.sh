5782d140df88d9214e07ac777ed4ddbadbdfda8f
martin-sicho <sicho.martin@gmail.com>
2020-05-30 21:15:30 +0200

    enable more seamless sharing of media files between containers and the host system

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,15 @@ set -e
 # compile the frontend app
 npm run-script build --prefix /code/src/genui_gui/
 
-# run the backend app
+# migrate the database and set everything up
 python manage.py migrate --noinput
 python manage.py genuisetup
 python manage.py collectstatic --no-input
-gunicorn --certfile=/etc/certs/${GENUI_BACKEND_HOST}.crt --keyfile=/etc/certs/${GENUI_BACKEND_HOST}.key genui.wsgi:application --bind 0.0.0.0:${GENUI_BACKEND_PORT}
+
+# ensure that only the genui user group has access to media files
+chgrp -R ${GENUI_USER_GROUP} /code/src/genui/media
+chmod g+rwxs,a+xs /code/src/genui/media # FIXME: here we allow all users to list directories and read the media files -> less permission headaches setting up the web server, but it is a security concern in some deployment settings
+umask 002
+
+# execute the command
+exec "$@"
