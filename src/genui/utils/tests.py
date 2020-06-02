87572e81a3cb8674cbc5b21324e0d789feefb0c8
martin-sicho <sicho.martin@gmail.com>
2020-06-02 09:56:52 +0200

    add the nvgpu utility

--- src/genui/utils/tests.py
+++ src/genui/utils/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from unittest import skipIf
+from . import gpu
 
-# Create your tests here.
+class GPUTests(TestCase):
+
+    @skipIf(not gpu.check_gpu_availability(), "No CUDA enabled gpus were found on the system. Skipping test...")
+    def test_info(self):
+        # TODO: implement
+        pass
