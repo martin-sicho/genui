6167bb0b4ba5e4a65c2a52eeea5f9ec3e94e6a2e
martin-sicho <sicho.martin@gmail.com>
2020-07-16 14:18:26 +0200

    use full path

--- src/genui/compounds/extensions/sdf/parser.py
+++ src/genui/compounds/extensions/sdf/parser.py
@@ -5,7 +5,7 @@ Created by: Martin Sicho
 On: 7/13/20, 2:49 PM
 """
 from rdkit import Chem
-from ..fileimports.parser import FileParser
+from genui.compounds.extensions.fileimports.parser import FileParser
 from . import models
 
 class SDFParser(FileParser):
