46360165c176861d7671255485f3a47b73bf81d1
martin-sicho <sicho.martin@gmail.com>
2020-12-23 11:20:13 +0100

    document the function

--- src/genui/about.py
+++ src/genui/about.py
@@ -1,5 +1,5 @@
 """
-version
+Version and related info.
 
 Created by: Martin Sicho
 On: 5/7/20, 2:25 PM
@@ -8,7 +8,8 @@ On: 5/7/20, 2:25 PM
 __all__ = [
     "__version__",
     "__devstatus__",
-    "__devversion__"
+    "__devversion__",
+    "get_release_info"
 ]
 
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#specifying-your-project-s-version
@@ -20,10 +21,22 @@ __devversion__ = "0" # prerelease version
 assert (__devstatus__ and __devversion__) or not (__devstatus__ or __devversion__)
 
 def get_release_info():
+    """
+    Generate an easy to use dictionary with release information.
+
+    Returns
+    -------
+    dict
+        a dictionary with release information
+    """
+
     version = __version__
     if __devstatus__ and __devversion__:
         version += f".{__devstatus__}{__devversion__}"
 
     return {
-        'version' : version
+        'version' : __version__,
+        'version_full' : version,
+        'dev_status' : __devstatus__,
+        'dev_version' : __devversion__
     }
